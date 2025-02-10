a7507d3bd1ee7dbdfc225283c8efd23b1534a0c8
Kostas Kokkoros <mavroprovato@gmail.com>
2025-02-10 04:39:07 +0200

    Add index controller

---
+++ src/main/kotlin/net/marvoprovato/hr/IndexController.kt
@@ -0,0 +1,10 @@
+package net.marvoprovato.hr
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class IndexController {
+    @GetMapping("/")
+    fun index() = "OK"
+}
