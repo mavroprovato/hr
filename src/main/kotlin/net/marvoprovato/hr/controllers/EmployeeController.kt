a12dc9c0c63de69e2c168023ee153b51292c507b
Kostas Kokkoros <mavroprovato@gmail.com>
2025-02-15 02:50:35 +0200

    Add read method

--- src/main/kotlin/net/marvoprovato/hr/controllers/EmployeeController.kt
+++ src/main/kotlin/net/marvoprovato/hr/controllers/EmployeeController.kt
@@ -1,7 +1,10 @@
 package net.marvoprovato.hr.controllers
 
+import net.marvoprovato.hr.Employee
 import net.marvoprovato.hr.services.EmployeeService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/employees")
 class EmployeeController(private val service: EmployeeService) {
     @GetMapping
-    fun listEmployees() = service.find()
+    fun list() = service.list()
+
+    @GetMapping("/{id}")
+    fun read(@PathVariable id: Int): ResponseEntity<Employee> = service.read(id).toResponseEntity()
+
+    private fun Employee?.toResponseEntity(): ResponseEntity<Employee> =
+        this?.let { ResponseEntity.ok(it) } ?: ResponseEntity.notFound().build()
 }
--- src/main/kotlin/net/marvoprovato/hr/services/EmployeeService.kt
+++ src/main/kotlin/net/marvoprovato/hr/services/EmployeeService.kt
@@ -3,10 +3,15 @@ package net.marvoprovato.hr.services
 import net.marvoprovato.hr.Employee
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.query
 
 @Service
 class EmployeeService(private val db: JdbcTemplate) {
-    fun find(): List<Employee> = db.query("select * from employees") { response, _ ->
+    fun list(): List<Employee> = db.query("select * from employees") { response, _ ->
         Employee(response.getInt("id"), response.getString("first_name"), response.getString("last_name"))
     }
+
+    fun read(id: Int): Employee? = db.query("select * from employees where id = ?", id) { response, _ ->
+        Employee(response.getInt("id"), response.getString("first_name"), response.getString("last_name"))
+    }.singleOrNull()
 }
