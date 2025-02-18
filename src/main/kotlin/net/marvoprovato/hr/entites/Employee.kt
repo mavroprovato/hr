75ff580e5e2139997a03b565ec111c0542d9f83b
Kostas Kokkoros <mavroprovato@gmail.com>
2025-02-18 04:09:03 +0200

    Add birth and hire data

--- src/main/kotlin/net/marvoprovato/hr/entites/Employee.kt
+++ src/main/kotlin/net/marvoprovato/hr/entites/Employee.kt
@@ -3,10 +3,15 @@ package net.marvoprovato.hr.entites
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDate
 
 @Entity
+@Table(name = "employees")
 class Employee(
     var firstName: String,
     var lastName: String,
+    var birthDate: LocalDate,
+    var hireDate: LocalDate,
     @Id @GeneratedValue var id: Long? = null
 )
