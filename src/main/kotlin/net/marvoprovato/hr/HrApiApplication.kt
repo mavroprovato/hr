2c96061b91c60013117a54f91cad7a2fbe00a311
Kostas Kokkoros <mavroprovato@gmail.com>
2025-02-26 00:03:50 +0200

    Rename application

--- src/main/kotlin/net/marvoprovato/hr/HrApiApplication.kt
+++ src/main/kotlin/net/marvoprovato/hr/HrApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HrApplication
+class HrApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<HrApplication>(*args)
+    runApplication<HrApiApplication>(*args)
 }
